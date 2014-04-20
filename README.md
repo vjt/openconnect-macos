5d607bbfaaaf0d1c6814dda30785695a3c7d6723
Marcello Barnaba <vjt@openssl.it>
2014-04-20 17:46:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Cisco VPN for nerds on MacOS
+
+This is a package containing an OpenConnect configuration script for MacOS
+that allows you to fully control how the VPN connection is made, which routes
+are added, and how the DNS are configured.
+
+If you are tired of having all your private traffic go through the corporate
+VPN, but you need it always up, this script is for you. It is MacOS and brew
+centric, but if you know your way you can of course make it work with your
+installation from source.
+
+Enjoy!
+
+## Setup
+
+ * Create `/usr/local/etc/vpnc` and enter into it
+
+        mkdir /usr/local/etc/vpnc
+        cd /usr/local/etc/vpnc
+
+ * Clone the sources into the `.source` directory
+
+        git clone https://github.com/vjt/openconnect-macos .source
+
+ * Install OpenConnect
+
+        brew install openconnect
+
+ * Install either the TUN driver via `brew install tuntap` or use the one bundled in [Tunnelblick](https://code.google.com/p/tunnelblick/)
+
+    * Brew provides you instructions on how to set up the tuntap driver. Follow them and then:
+
+             ln -s /Library/Extensions/tun.kext .
+
+    * For TunnelBlick bundled driver:
+
+             ln -s /Applications/Tunnelblick.app/Contents/Resources/tun.kext .
+
+ * Copy the configuration file and set your username in it:
+
+        cp .source/vpn.conf myvpn.conf
+        vi myvpn.conf
+
+ * Copy the network configuration script:
+
+        cp .source/vpn.sh myvpn.sh
+
+ * Grant yourself rights to start the VPN without password: run `sudo visudo` and add
+
+        %admin ALL=(ALL) NOPASSWD: /usr/local/bin/openconnect
+
+   at the end of the file.
+
+ * Done!
+
+## Connect the VPN
+
+    openconnect --config /usr/local/etc/myvpn.conf concentrator.example.com
+
+## Disconnect the VPN
+
+    Ctrl+C
+
+## Advantages
+
+ * Start the VPN straight from the Command Line
+ * Only DNS requests for the corporate domain are routed through the VPN
+ * Only corporate traffic is routed through the VPN, for more privacy, security, and reliability.
---
+++ vpn.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+#* reason                       -- why this script was called, one of: pre-init connect disconnect
+#* VPNGATEWAY                   -- vpn gateway address (always present)
+#* TUNDEV                       -- tunnel device (always present)
+#* INTERNAL_IP4_ADDRESS         -- address (always present)
+#* INTERNAL_IP4_NETMASK         -- netmask (often unset)
+#* INTERNAL_IP4_NETMASKLEN      -- netmask length (often unset)
+#* INTERNAL_IP4_NETADDR         -- address of network (only present if netmask is set)
+#* INTERNAL_IP4_DNS             -- list of dns serverss
+#* INTERNAL_IP4_NBNS            -- list of wins servers
+#* CISCO_DEF_DOMAIN             -- default domain name
+#* CISCO_BANNER                 -- banner from server
+#* CISCO_SPLIT_INC              -- number of networks in split-network-list
+#* CISCO_SPLIT_INC_%d_ADDR      -- network address
+#* CISCO_SPLIT_INC_%d_MASK      -- subnet mask (for example: 255.255.255.0)
+#* CISCO_SPLIT_INC_%d_MASKLEN   -- subnet masklen (for example: 24)
+#* CISCO_SPLIT_INC_%d_PROTOCOL  -- protocol (often just 0)
+#* CISCO_SPLIT_INC_%d_SPORT     -- source port (often just 0)
+#* CISCO_SPLIT_INC_%d_DPORT     -- destination port (often just 0)
+
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+
+# Override DNS servers, if needed
+INTERNAL_IP4_DNS="10.0.0.1 10.0.0.2"
+
+# Specify here the routes you want to add
+INTERNAL_ROUTES="10.42.0.0/24 172.31.33.0/24"
+
+# Specify here the service name, if you want to run multiple VPNs at the same time.
+SERVICE_NAME="org.foobar.myvpn"
+
+configure_iface () {
+  ifconfig "$TUNDEV" inet "$INTERNAL_IP4_ADDRESS" "$INTERNAL_IP4_ADDRESS" \
+    netmask 255.255.255.255 mtu ${INTERNAL_IP4_MTU:-1412} up
+}
+
+set_routes() {
+  for route in $INTERNAL_ROUTES; do
+    route add "$route" -iface "$TUNDEV" >/dev/null
+  done
+}
+
+set_dns() {
+  scutil <<EOF
+    d.init
+    d.add Addresses * $INTERNAL_IP4_ADDRESS
+    d.add DestAddresses * $INTERNAL_IP4_ADDRESS
+    d.add InterfaceName $TUNDEV
+    set State:/Network/Service/$SERVICE_NAME/IPv4
+
+    d.init
+    d.add SupplementalMatchDomains * $CISCO_DEF_DOMAIN
+    d.add ServerAddresses * $INTERNAL_IP4_DNS
+    set State:/Network/Service/$SERVICE_NAME/DNS
+EOF
+}
+
+unset_dns() {
+  scutil <<EOF
+    remove State:/Network/Service/$SERVICE_NAME/DNS
+    remove State:/Network/Service/$SERVICE_NAME/IPv4
+EOF
+}
+
+load_tun_kext() {
+  if ! kextstat | grep -q '\.tun '; then
+    kextload `dirname $0`/tun.kext
+  fi
+}
+
+case "$reason" in
+  pre-init)
+    load_tun_kext
+    ;;
+  connect)
+    mkdir -p /var/run/vpnc
+    configure_iface
+    set_dns
+    set_routes
+    ;;
+  disconnect)
+    unset_dns
+    ;;
+  *)
+    exit 0
+    ;;
+esac
+
+exit 0
